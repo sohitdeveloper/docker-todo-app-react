e808b867c1e4e396b57335d20d8f626396dac1de
Sohit <sohit@digitalgreen.org>
2023-08-22 14:31:07 +0530

    ut file added.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todoText, setTodoText] = useState("");
+
+  const handleAddTodo = () => {
+    if (todoText.trim() !== "") {
+      setTodos([...todos, { id: Date.now(), text: todoText }]);
+      setTodoText("");
+    }
+  };
+
+  const handleDeleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "20px" }}>
+      <TextField
+        label="Add a task"
+        variant="outlined"
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleAddTodo}
+        style={{ marginLeft: "10px" }}
+      >
+        Add
+      </Button>
+
+      <List style={{ marginTop: "20px" }}>
+        {todos.map((todo) => (
+          <ListItem
+            key={todo.id}
+            sx={{
+              boxShadow:
+                "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
+            }}
+          >
+            <ListItemText primary={todo.text} />
+            <ListItemSecondaryAction>
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                onClick={() => handleDeleteTodo(todo.id)}
+                sx={{
+                  ":hover": {
+                    color: "red",
+                  },
+                }}
+                data-testid="delete_btn"
+              >
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+describe("Todo module", () => {
+  test("renders TodoApp component", () => {
+    render(<App />);
+    const inputEle = screen.getByLabelText("Add a task");
+    const addBtn = screen.getByRole("button", { name: "Add" });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    expect(inputEle).toBeInTheDocument();
+    expect(addBtn).toBeInTheDocument();
+  });
+
+  test("add a new task", () => {
+    render(<App />);
+    const inputEle = screen.getByLabelText("Add a task");
+    const addBtn = screen.getByRole("button", { name: "Add" });
+
+    fireEvent.change(inputEle, { target: { value: "learn rtl" } });
+    fireEvent.click(addBtn);
+
+    const todoItem = screen.getByText("learn rtl");
+    expect(todoItem).toBeInTheDocument();
+  });
+
+  test("delete a task", () => {
+    render(<App />);
+    const inputEle = screen.getByLabelText("Add a task");
+    const addBtn = screen.getByRole("button", { name: "Add" });
+
+    fireEvent.change(inputEle, { target: { value: "learn docker" } });
+    fireEvent.click(addBtn);
+
+    const todoItem = screen.getByText("learn docker");
+    expect(todoItem).toBeInTheDocument();
+
+    const deleteBtn = screen.getByTestId("delete_btn");
+    fireEvent.click(deleteBtn);
+
+    expect(todoItem).not.toBeInTheDocument();
+  });
 });
